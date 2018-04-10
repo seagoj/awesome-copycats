3ae864f395460d342c9e8e9b7df57f5ba35e7a16
Jeremy Seago <seagoj@gmail.com>
2018-04-09 19:32:14 -0500

    set default layout

--- rc.lua
+++ rc.lua
@@ -87,11 +87,11 @@ local scrlocker    = "xlock"
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
+    awful.layout.suit.floating,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
